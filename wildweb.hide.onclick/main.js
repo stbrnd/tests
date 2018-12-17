012accc18a4f8ac50e2993d7991b95b4efb8dddd
Yaroslav <y.surilov@infomir.com>
2018-12-17 11:30:13 +0200

    remove redundant comments

--- wildweb.hide.onclick/main.js
+++ wildweb.hide.onclick/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// DEBUG
 console.log('start --->');
 console.log(stbWindowMgr.windowList());
 
@@ -9,12 +8,10 @@ window.resizeTo(screen.width - 500, screen.height - 300);
 
 stbWindowMgr.openWebFace('http://stbrnd.github.io/tests/wildweb.hide.onclick/web.face.html');
 
-// DEBUG
 console.log('\ninit and hide web face --->');
 console.log(stbWindowMgr.windowList());
 
 document.addEventListener('click', function () {
-    // DEBUG
     console.log('\nclick in portal window --->');
     console.log(stbWindowMgr.windowList());
 
